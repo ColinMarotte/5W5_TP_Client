c55381a4b4aa27bc6281b5ffa02f70af9d2e35f3
David Bourgeois <1192146@cegepmontpetit.ca>
2025-04-07 11:37:33 -0400

    FCT ajout de la gestion du coût d'une carte avant de la jouer.

--- ngsci-depart-main/src/app/match/playerhand/playerhand.component.ts
+++ ngsci-depart-main/src/app/match/playerhand/playerhand.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PlayableCard } from 'src/app/models/models';
+import { Card, PlayableCard } from 'src/app/models/models';
 import { CardComponent } from '../../components/card/card.component';
 import { MatchService } from 'src/app/services/match.service';
 
@@ -12,17 +12,28 @@ import { MatchService } from 'src/app/services/match.service';
     imports: [CardComponent]
 })
 export class PlayerhandComponent implements OnInit {
+  public playerMana: number = 0; // Mana du joueur
 
   @Input() cards: PlayableCard[] = [];
 
   constructor(public matchService: MatchService) { }
 
   ngOnInit() {
-
+    this.playerMana = this.matchService.playerData?.mana ?? 0; // Récupérer la mana du joueur
+    
   }
 
-  async click(playableCardId:any){
+  async click(playableCard:any){
+    let player = this.matchService.playerData;
+    if(!player) {
+      console.log("Erreur: Impossible de trouver les données du joueur.");
+      return;
+    }
+    if(player.mana < playableCard.card.cost) {
+      console.log("Pas assez de mana pour jouer cette carte !");
+      return;
+    }
     // TODO: Utiliser seulement une fois que l'on peut jouer des cartes (TP2)
-    await this.matchService.playCard(playableCardId);
+    await this.matchService.playCard(playableCard.id);
   }
 }
