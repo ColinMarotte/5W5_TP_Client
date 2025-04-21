33ac32387b1d6d2f52c0e28559e6475a6b8d2d42
adjihane <2282988@cegepmontpetit.ca>
2025-04-21 00:18:22 -0400

    FCT : Animations

--- ngsci-depart-main/src/app/components/card/card.component.ts
+++ ngsci-depart-main/src/app/components/card/card.component.ts
@@ -44,102 +44,31 @@ export class CardComponent implements OnInit {
   beautifulBackUrl = "https://i.pinimg.com/236x/3c/73/0d/3c730d6df70700a3c912a3c87d6d2027.jpg";
   animatedPowerIndex: number = -1;
   animationClass: string = '';
+  animationCounters: number[] = [];
 
-  css_classanimation = false;
+  hoveredPowerIndex: number = -1;
 
   constructor() { }
 
-  ngOnInit() { }
-
-  count = 0;
-  animatedIndex = -1;
-
-  triggerPowerAnimation(i: number) {
-    this.animatedIndex = i;
-    this.count++; // Déclenche la transition ':increment'
-  }
-  getAnimationClass(powerName: string): string {
-    switch (powerName) {
-      case 'First Strike':
-        return 'wobble-hor-bottom';
-      case 'Thorns':
-        return 'rotate-scale-up-ver';
-      case 'Heal':
-        return 'bounce-in-fwd';
-      case 'Shield':
-        return 'rotate-in-center';
-      default:
-        return 'rotate-in-center';
+  ngOnInit() {
+    if (this.card?.cardPowers) {
+      this.animationCounters = new Array(this.card.cardPowers.length).fill(0);
     }
   }
 
-  animation(i: number) {
-    if (!this.card?.cardPowers || !this.card.cardPowers[i]?.power?.name)
-      return;
+  count = 0;
+  animatedIndex = -1;
+  async playAllPowerAnimations() {
+    if (!this.card?.cardPowers) return;
 
-    const powerName = this.card.cardPowers[i].power.name;
-    const className = this.getAnimationClass(powerName);
-
-    // On retire la classe
-    this.css_classanimation = false;
-    this.animationClass = '';
-
-
-    setTimeout(() => {
-      this.animationClass = className;
-      this.css_classanimation = true;
-
-
-      setTimeout(() => {
-        this.css_classanimation = false;
-        this.animationClass = '';
-      }, 2000); // Durée de ton animation en ms
-    });
+    for (let i = 0; i < this.card.cardPowers.length; i++) {
+      this.animationCounters[i]++;
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+    }
   }
 
+  animations(i: number) {
+    this.playAllPowerAnimations();
+  }
 
-  // Utilisation de async/await pour gérer l'attente des animations
-  // async animationsPouvoirs() {
-  //   if (!this.card?.cardPowers) return;
-
-  //   for (let i = 0; i < this.card.cardPowers.length; i++) {
-  //     const powerName = this.card.cardPowers[i].power.name;
-  //     this.animationClass = this.getAnimationClass(powerName);
-  //     this.animatedPowerIndex = i;
-
-  //     // Attente de 500 ms avant de passer à l'animation suivante
-  //     await this.waitFor(0.5);
-  //   }
-
-  //   // Réinitialisation après toutes les animations
-  //   await this.waitFor(0.5);  // Attente supplémentaire si nécessaire
-  //   this.animatedPowerIndex = -1;
-  //   this.animationClass = '';
-  // }
-
-  // // Fonction pour gérer les délais (en secondes)
-  // async waitFor(delayInSeconds: number) {
-  //   await lastValueFrom(timer(delayInSeconds * 1000));
-  // }
-
-
-  // async doubleCenterSpin(index?: number) {
-  //   if (index !== undefined) {
-
-  //     this.animatedPowerIndex = index;
-  //     const powerName = this.card?.cardPowers?.[index].power.name || '';
-  //     this.animationClass = this.getAnimationClass(powerName);
-  //     console.log(powerName + this.animatedPowerIndex)
-
-  //     // Attente de 2 secondes pour cette animation
-  //     await this.waitFor(2);  // Attente de 2 secondes avant de réinitialiser l'index
-  //     this.animatedPowerIndex = -1;
-  //     this.animationClass = '';
-  //   } else {
-  //     // Animation Wobble avec délai
-  //     this.css_wobbleBottom = true;
-  //     await this.waitFor(2);  // Attente de 2 secondes avant de réinitialiser
-  //     this.css_wobbleBottom = false;
-  //   }
-  // }
 }
