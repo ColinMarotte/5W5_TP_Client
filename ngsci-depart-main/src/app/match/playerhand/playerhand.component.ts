7b820b2c2b20f07bd0e30841b163911801a2a37b
j <2282988@cegepmontpetit.ca>
2025-04-28 10:22:33 -0400

    PROGRESS : Animation carte selectionnée

--- ngsci-depart-main/src/app/match/playerhand/playerhand.component.ts
+++ ngsci-depart-main/src/app/match/playerhand/playerhand.component.ts
@@ -2,43 +2,65 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Card, PlayableCard } from 'src/app/models/models';
 import { CardComponent } from '../../components/card/card.component';
 import { MatchService } from 'src/app/services/match.service';
+import { transition, trigger, useAnimation } from '@angular/animations';
+import { flip } from 'ng-animate';
 
 
 @Component({
-    selector: 'app-playerhand',
-    templateUrl: './playerhand.component.html',
-    styleUrls: ['./playerhand.component.css'],
-    standalone: true,
-    imports: [CardComponent]
+  selector: 'app-playerhand',
+  templateUrl: './playerhand.component.html',
+  styleUrls: ['./playerhand.component.css'],
+  standalone: true,
+  imports: [CardComponent],
+  animations: [
+    trigger('trigger', [
+      transition(':increment', useAnimation(flip, {
+        params: { timing: 2 }
+      }))
+    ]),
+  ]
 })
 export class PlayerhandComponent implements OnInit {
   public playerMana: number = 0; // Mana du joueur
 
   @Input() cards: PlayableCard[] = [];
-
+  mavar = 0;
+  trigger = false;
   constructor(public matchService: MatchService) { }
 
   ngOnInit() {
     this.playerMana = this.matchService.playerData?.mana ?? 0; // Récupérer la mana du joueur
-    
+
   }
 
-  async click(playableCard:PlayableCard){
+  async click(playableCard: PlayableCard) {
     let player = this.matchService.playerData;
-    if(!player) {
+    if (!player) {
       console.log("Erreur: Impossible de trouver les données du joueur.");
       return;
     }
-    if(player.mana < playableCard.card.cost) {
+    if (player.mana < playableCard.card.cost) {
       console.log("Pas assez de mana pour jouer cette carte !");
       return;
     }
     // TODO: Utiliser seulement une fois que l'on peut jouer des cartes (TP2)
-    if(this.matchService.isCurrentPlayerTurn){
+    if (this.matchService.isCurrentPlayerTurn) {
       await this.matchService.playCard(playableCard.id);
+      this.playAnimation();
+
     }
-    else{
+    else {
       console.log("Ce n'est pas ton tour!");
     }
+
+
   }
+
+  playAnimation() {
+    this.mavar++;  // Change la variable pour déclencher l'animation
+    setTimeout(() => {
+      this.mavar++;  // Incrémente encore une fois après un délai pour la finir
+    }, 1000);
+  }
+
 }
