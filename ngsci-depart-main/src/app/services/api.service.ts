72cd0565dfb6562747fca4f7a46dd194a1c19ae4
David Bourgeois <1192146@cegepmontpetit.ca>
2025-05-26 09:01:21 -0400

    FCT ajout de environments

--- ngsci-depart-main/src/app/services/api.service.ts
+++ ngsci-depart-main/src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Card, CardPower } from '../models/models';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
